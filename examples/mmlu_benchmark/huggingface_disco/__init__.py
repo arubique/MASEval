b7ad594d691b4b56ee61148221ccb4fd2aecc4cd
Alexander Rubinstein <rubalex14@gmail.com>
2026-02-14 09:44:08 +0100

    [Create PR]: - Remove extra files

--- examples/mmlu_benchmark/huggingface_disco/__init__.py
+++
@@ -1,6 +0,0 @@
-# DISCO predictor as a Hugging Face custom model.
-
-from .configuration_disco import DiscoConfig
-from .modeling_disco import DiscoPredictor
-
-__all__ = ["DiscoConfig", "DiscoPredictor"]
